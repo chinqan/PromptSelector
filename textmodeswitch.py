9e713d69bed2e4dd531a1acfc6dcedc39d222232
chinqan <chinqan@gmail.com>
2026-02-25 19:07:08 +0800

    Update textmodeswitch.py

--- textmodeswitch.py
+++ textmodeswitch.py
@@ -8,22 +8,22 @@ class TextModeSwitch:
         return {
             "required": {
                 "mode": (["手動調整", "自動生成"], {"default": "手動調整"}),
-                "textA1": ("STRING", {"forceInput": True}),
-                "textA2": ("STRING", {"forceInput": True}),
-                "textB1": ("STRING", {"forceInput": True}),
-                "textB2": ("STRING", {"forceInput": True}),
+                "prompt1": ("STRING", {"forceInput": True}),
+                "log1": ("STRING", {"forceInput": True}),
+                "prompt2": ("STRING", {"forceInput": True}),
+                "log2": ("STRING", {"forceInput": True}),
             },
         }
 
     RETURN_TYPES = ("STRING", "STRING")
-    RETURN_NAMES = ("text1", "text2")
+    RETURN_NAMES = ("prompt", "log")
     FUNCTION = "switch_logic"
     CATEGORY = "MyCustomNodes"
 
-    def switch_logic(self, mode, textA1, textA2, textB1, textB2):
-        if mode == "Mode_A":
+    def switch_logic(self, mode, prompt1, log1, prompt2, log2):
+        if mode == "模式":
             # 模式 A：將 A1, A2 導向輸出
-            return (textA1, textA2)
+            return (prompt1, log1)
         else:
             # 模式 B：將 B1, B2 導向輸出
-            return (textB1, textB2)
+            return (prompt2, log2)
