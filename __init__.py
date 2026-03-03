7dc0fc3bc7f4218691c4482497a6819f371b9a24
chinqan <chinqan@gmail.com>
2026-03-03 22:48:07 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -204,10 +204,10 @@ class MasterOutfitSelector:
         
         final_prompt = ", ".join(all_parts)
         
-        debug_parts = [""]
+        debug_parts = ""
         for o in [o_hair, o_tops, o_bottoms, o_shoes, o_acc, o_bags, o_neck, o_wrist]:
             if o:
-                debug_parts.append(o)
+                debug_parts = debug_parts + o
         
         return (o_hair, o_tops, o_bottoms, o_shoes, o_acc, o_bags, o_neck, o_wrist, debug_parts, final_prompt)
 
